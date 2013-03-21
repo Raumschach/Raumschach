401459ed0340baf5aaa69b6196513f768784a05b
Dan Iverson <iversoda@rose-hulman.edu>
2013-03-20 22:05:26 -0400

    here goes nothing

---
+++ Raumschach/Raumschach/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Raumschach")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Rose-Hulman Institute of Technology")> 
+<Assembly: AssemblyProduct("Raumschach")> 
+<Assembly: AssemblyCopyright("Copyright © Rose-Hulman Institute of Technology 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0aad2ea7-549b-49be-8709-e134453b2a51")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
