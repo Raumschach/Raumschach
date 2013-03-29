7723ee4c44c71d762414799f57da6c1c622617d1
Dan Iverson <iversoda@rose-hulman.edu>
2013-03-28 20:20:33 -0400

    seperated tests

---
+++ Raumschach/RaumschachTests/BoardTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NUnit.Framework;
+using System.Windows.Forms;
+using RaumschachForm;
+using System.Collections.Generic;
+
+namespace RaumschachTests
+{
+    [TestFixture()]
+    public class BoardTests
+    {
+        [Test()]
+        public void TestNewBoard(){
+            var target = new RaumschachForm.Board();
+            var cell = new RaumschachForm.Cell("Aa1");
+            cell.addPiece(new Pawn(true, cell.getName()));
+            var boardCell = target._board[0][0, 0];
+            Assert.IsTrue(cell.getPiece().Equals( boardCell.getPiece()));
+            
+        }
+        [Test()]
+        public void MovePieceToEmpty()
+        {
+            var target = new RaumschachForm.Board();
+            target.movePiece("Aa1", "Aa2");
+            Assert.IsNull(target._board[0][0, 0].getPiece());
+            Assert.IsTrue(new Pawn(true, "Aa2").Equals(target._board[0][0, 1].getPiece()));
+        }
+        [Test()]
+        public void MovePieceFromNullToNull()
+        {
+            var target = new RaumschachForm.Board();
+            target.movePiece("Aa3", "Aa2");
+            Assert.IsTrue(new Pawn(true, "Aa1").Equals(target._board[0][0, 0].getPiece()));
+            Assert.IsNull(target._board[0][0, 1].getPiece());
+            Assert.IsNull(target._board[0][0, 2].getPiece());
+        }
+        [Test()]
+        public void MovePieceFromNullToPiece()
+        {
+            var target = new RaumschachForm.Board();
+            target.movePiece("Aa2", "Aa1");
+            Assert.IsTrue(new Pawn(true, "Aa1").Equals( target._board[0][0, 0].getPiece()));
+            Assert.IsNull(target._board[0][0, 1].getPiece());
+        }
+        [Test()]
+        public void MovePieceToItself()
+        {
+            var target = new RaumschachForm.Board();
+            target.movePiece("Aa1", "Aa1");
+            Assert.IsTrue(new Pawn(true, "Aa1").Equals( target._board[0][0, 0].getPiece()));
+        }
+        [Test()]
+        public void MovePieceToPiece()
+        {
+            var target = new RaumschachForm.Board();
+            target.movePiece("Aa1", "Ab1");
+            Assert.IsNull(target._board[0][0, 0].getPiece());
+            Assert.IsTrue(new Pawn(true, "Ab1").Equals(target._board[0][1, 0].getPiece()));
+
+        }
+        
+    }
+}
---
+++ Raumschach/RaumschachTests/PawnTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+using NUnit.Framework;
+using RaumschachForm;
+using System.Collections.Generic;
+using Assert = NUnit.Framework.Assert;
+
+namespace RaumschachTests
+{
+    [TestClass]
+    public class PawnTests
+    {
+             [Test()]
+        public void TestnewPawn()
+        {
+            var target = new Pawn(true, "test");
+            Assert.IsNotNull(target);
+            Assert.IsTrue(target.white);
+
+        }
+        [Test()]
+        public void TestPiecegetMoves()
+        {
+           var target = new Pawn(true, "Ee4");
+           var board = new Board();
+            var tempList = new List<String> {"De4", "Ee3"};
+            Assert.AreEqual(tempList, target.getmoves(board));
+           
+
+        }
+        [Test()]
+        public void TestwhitePawnTakeUpRight()
+        {
+            var target = new RaumschachForm.Pawn(true, "Dc4");
+            var board = new RaumschachForm.Board();
+            board.getCell("Cb4").addPiece(new Pawn(false, "Cb4"));
+            var tempList = new List<String> {"Cc4", "Dc3", "Cb4"};
+            Assert.AreEqual(tempList, target.getmoves(board));
+        }
+        [Test()]
+        public void TestwhitePawnTakeUpLeft()
+        {
+            var target = new RaumschachForm.Pawn(true, "Dc4");
+            var board = new RaumschachForm.Board();
+            board.getCell("Cd4").addPiece(new Pawn(false, "Cd4"));
+            var tempList = new List<String> {"Cc4", "Dc3", "Cd4"};
+            Assert.AreEqual(tempList, target.getmoves(board));
+        }
+        [Test()]
+        public void TestwhitePawnTakeForwardRight()
+        {
+            var target = new RaumschachForm.Pawn(true, "Dc4");
+            var board = new RaumschachForm.Board();
+            board.getCell("Db3").addPiece(new Pawn(false, "Db3"));
+            var tempList = new List<String> {"Cc4", "Dc3", "Db3"};
+            Assert.AreEqual(tempList, target.getmoves(board));
+        }
+        [Test()]
+        public void TestwhitePawnTakeForwardLeft()
+        {
+            var target = new RaumschachForm.Pawn(true, "Dc4");
+            var board = new RaumschachForm.Board();
+            board.getCell("Dd3").addPiece(new Pawn(false, "Dd3"));
+            var tempList = new List<String> {"Cc4", "Dc3", "Dd3"};
+            Assert.AreEqual(tempList, target.getmoves(board));
+        }
+        [Test()]
+        public void TestBlackPawnGetMoves()
+        {
+            var target = new RaumschachForm.Pawn(false, "Bd2");
+            var board = new RaumschachForm.Board();
+            var tempList = new List<String> {"Cd2", "Bd3"};
+            Assert.AreEqual(tempList, target.getmoves(board));
+        }
+        [Test()]
+        public void TestBlackPawnTakeDownRight()
+        {
+            var target = new RaumschachForm.Pawn(false, "Bd2");
+            var board = new RaumschachForm.Board();
+            board.getCell("Ce2").addPiece(new Pawn(true, "Ce2"));
+            var tempList = new List<String> {"Cd2", "Bd3", "Ce2"};
+            Assert.AreEqual(tempList, target.getmoves(board));
+        }
+        [Test()]
+        public void TesBlackPawnTakeDownLeft()
+        {
+            var target = new RaumschachForm.Pawn(false, "Bd2");
+            var board = new RaumschachForm.Board();
+            board.getCell("Cc2").addPiece(new Pawn(true, "Cc2"));
+            var tempList = new List<String> {"Cd2", "Bd3", "Cc2"};
+            Assert.AreEqual(tempList, target.getmoves(board));
+        }
+        [Test()]
+        public void TestBlackPawnTakeForwardRight()
+        {
+            var target = new RaumschachForm.Pawn(false, "Bd2");
+            var board = new RaumschachForm.Board();
+            board.getCell("Be3").addPiece(new Pawn(true, "Be3"));
+            var tempList = new List<String> {"Cd2", "Bd3", "Be3"};
+            Assert.AreEqual(tempList, target.getmoves(board));
+        }
+        [Test()]
+        public void TestBlackPawnTakeForwardLeft()
+        {
+            var target = new RaumschachForm.Pawn(false, "Bd2");
+            var board = new RaumschachForm.Board();
+            board.getCell("Bc3").addPiece(new Pawn(true, "Bc3"));
+            var tempList = new List<String> {"Cd2", "Bd3", "Bc3"};
+            Assert.AreEqual(tempList, target.getmoves(board));
+        }
+        
+    }
+}
