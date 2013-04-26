f754aff7903c7c653942225546e32c1b9153b20b
Teddy <sternetj@rose-hulman.edu>
2013-04-25 22:21:22 -0400

    Completed Knight piece and tests

---
+++ Raumschach/RaumschachForm/Knight.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaumschachForm
+{
+    //It's not the piece Gotham wants, it's the piece Gotham need
+    //It's the Dark Knight
+    public class Knight: Piece
+    {
+
+#if (DEBUG && !MYTEST)
+        public readonly Image BlackKnight = Image.FromFile
+            (Environment.CurrentDirectory + @"\Images\KnightB.png"); //Bruce Wayne
+
+        public readonly Image WhiteKnight = Image.FromFile(Environment.CurrentDirectory + @"\Images\KnightW.png"); //THarvey Two Face
+        #endif
+        #if (DEBUG && MYTEST)
+                    public readonly Image BlackPawn = Image.FromFile
+                //  (@"C:\Users\iversoda\Documents\SQA\Project\Raumschach\Raumschach\RaumschachForm\bin\Debug\Images\KnightB.png");
+            (@"C:\Users\sternetj\Documents\GitHub\Raumschach\Raumschach\RaumschachForm\bin\Debug\Images\KnightB.png");
+            public readonly Image WhitePawn = Image.FromFile
+            //(@"C:\Users\iversoda\Documents\SQA\Project\Raumschach\Raumschach\RaumschachForm\bin\Debug\Images\KnightW.png");
+            (@"C:\Users\sternetj\Documents\GitHub\Raumschach\Raumschach\RaumschachForm\bin\Debug\Images\KnightW.png");
+        #endif
+
+        public Knight(bool white, string currentPos)
+        {
+            White = white;
+            CurrentPos = currentPos;
+        }
+
+        public override List<string> GetMoves(Board board)
+        {
+            var moves = new List<string>();
+
+            foreach (var mainDirc in Enum.GetValues(typeof(Board.CellNeighbor)))
+            {
+                var mainCell = board.GetNeighborCell(board.GetCell(CurrentPos), (Board.CellNeighbor)mainDirc);
+                if (mainCell == null) continue;
+                mainCell = board.GetNeighborCell(mainCell, (Board.CellNeighbor)mainDirc);
+                if (mainCell == null) continue;
+                foreach(var secondaryDirc in Enum.GetValues(typeof(Board.CellNeighbor)))
+                {
+                    var checkCell = board.GetNeighborCell(mainCell, (Board.CellNeighbor)secondaryDirc);
+
+                    if (board.IsSameOrOpposite((Board.CellNeighbor)mainDirc, (Board.CellNeighbor)secondaryDirc) ||
+                        checkCell == null || (checkCell.GetPiece() != null && checkCell.GetPiece().White == this.White)) continue;
+                    moves.Add(checkCell.GetName());
+
+                }
+            }
+
+            return moves;
+        }
+
+        public override Image GetImage()
+        {
+            return White ? WhiteKnight : BlackKnight;
+        }
+    }
+}
---
+++ Raumschach/RaumschachTests/KnightTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using RaumschachForm;
+using System.Collections.Generic;
+using Assert = NUnit.Framework.Assert;
+
+namespace RaumschachTests
+{
+    [TestFixture]
+    public class KnightTests
+    {
+        private Board _board;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _board = new Board();
+        }
+        [Test]
+        public void TestNewKnight()
+        {
+            var target = new Knight(true, "test");
+            Assert.IsNotNull(target);
+            Assert.IsTrue(target.White);
+        }
+
+        [Test]
+        public void TestKnightGetMoves()
+        {
+            var target = new Knight(true, "Cc3");
+            var movesList = new List<String> { "Cb5", "Cd5", "Bc5", "Dc5", "Cb1", "Cd1", "Bc1", "Dc1", "Ca4", "Ca2", "Ba3", "Da3", "Ce4", "Ce2", "Be3", "De3", "Ac4", "Ac2", "Ab3", "Ad3", "Ec4", "Ec2", "Eb3", "Ed3"};
+            Assert.AreEqual(movesList, target.GetMoves(_board));
+        }
+        [Test]
+        public void TestKnightInCorner()
+        {
+            var target = new Knight(true, "Aa1");
+            var movesList = new List<String> { "Ab3", "Ba3", "Ac2", "Bc1", "Ca2", "Cb1"};
+            Assert.AreEqual(movesList, target.GetMoves(_board));
+        }
+        [Test]
+        public void TestKnightWithBlockingPiece()
+        {
+            var target = new Knight(true, "Aa1");
+            var target2 = new Pawn(true, "Bc1");
+            var movesList = new List<String> { "Ab3", "Ba3", "Ac2", "Ca2", "Cb1" };
+            _board.GetCell(target2.CurrentPos).AddPiece(target2);
+            Assert.AreEqual(movesList, target.GetMoves(_board));
+        }
+        [Test]
+        public void TestKnightWithTakeablePiece()
+        {
+            var target = new Knight(true, "Aa1");
+            var target2 = new Pawn(false, "Bc1");
+            var movesList = new List<String> { "Ab3", "Ba3", "Ac2", "Bc1", "Ca2", "Cb1" };
+            _board.GetCell(target2.CurrentPos).AddPiece(target2);
+            Assert.AreEqual(movesList, target.GetMoves(_board));
+        }
+    }
+}
